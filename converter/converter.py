a7f238cdde3cc3c4aea9c60dd5eb67f1e8090caf
Cody Brocious <cody.brocious@gmail.com>
2016-10-11 16:59:29 -0600

    Added S3D fallback for old zones.

--- converter/converter.py
+++ converter/converter.py
@@ -7,12 +7,25 @@ from wld import readWld
 from zon import readZon
 from zonefile import *
 
+def s3dFallback(*filedicts):
+    ndicts = []
+    for files in filedicts:
+        nfiles = {}
+        ndicts.append(nfiles)
+        for xfiles in filedicts:
+            if xfiles is not files:
+                nfiles.update(xfiles)
+        nfiles.update(files)
+    return ndicts
+
 def convertOld(name):
     with ZipFile('%s.zip' % name, 'w') as zip:
         zone = Zone()
         ofiles = readS3D(file('eqdata/%s_obj.s3d' % name, 'rb'))
-        readWld(ofiles['%s_obj.wld' % name], zone, ofiles, isZone=False)
         zfiles = readS3D(file('eqdata/%s.s3d' % name, 'rb'))
+        ofiles, zfiles = s3dFallback(ofiles, zfiles)
+
+        readWld(ofiles['%s_obj.wld' % name], zone, ofiles, isZone=False)
         readWld(zfiles['objects.wld'], zone, zfiles, isZone=False)
         readWld(zfiles['%s.wld' % name], zone, zfiles, isZone=True)
         zone.output(zip)
@@ -21,6 +34,8 @@ def convertNew(name):
     with ZipFile('%s.zip' % name, 'w') as zip:
         zone = Zone()
         zfiles = readS3D(file('eqdata/%s.eqg' % name, 'rb'))
+        #for fn, data in zfiles.items():
+        #    file('s3data/%s' % fn, 'wb').write(data)
         readZon(zfiles['%s.zon' % name], zone, zfiles)
         zone.output(zip)
 
