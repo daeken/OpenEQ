ed1ceba843eb4d87217ce4d5dc7fd4e05a409603
Cody Brocious <cody.brocious@gmail.com>
2018-07-31 08:03:56 -0400

    Greatly expanded wld reading and debug output.

--- LegacyFileReader/Debugging.cs
+++ LegacyFileReader/Debugging.cs
@@ -2,9 +2,10 @@
 
 namespace OpenEQ.LegacyFileReader {
 	public static class Debugging {
+		static string Escape(string v) => v.Replace("&", "&amp;").Replace("\"", "&quot;").Replace("<", "&lt;").Replace(">", "&gt;");
 		public static void OutputHTML(Wld wld) {
-			foreach(var frag in wld.Fragments) {
-				
+			foreach(var (name, frag) in wld.Fragments) {
+				WriteLine($"<li>{(string.IsNullOrEmpty(name) ? "" : $"<i>{Escape(name)}</i> - ")}{Escape(frag?.ToString() ?? "NULL")}</li>");
 			}
 		}
 	}
--- LegacyFileReader/Extensions.cs
+++ LegacyFileReader/Extensions.cs
@@ -1,11 +1,21 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using OpenEQ.Common;
 
 namespace OpenEQ.LegacyFileReader {
 	public static class Extensions {
-		public static string Stringify<T>(this T[] arr) => $"[{string.Join(", ", arr.Select(x => x.ToString()))}]";
+		public static string Stringify<T>(this T[] arr) => arr == null ? "null" : $"[{string.Join(", ", arr.Select(x => x.ToString()))}]";
 		
 		public static Reference<T> ReadRef<T>(this BinaryReader br, Wld wld) where T : class => new Reference<T>(wld, br.ReadInt32());
+
+		public static bool HasBit(this uint value, int bit) => (value & (1 << bit)) != 0;
+
+		public static IEnumerable<int> Times(this int count) => Enumerable.Range(0, count);
+		public static IEnumerable<int> Times(this uint count) => ((int) count).Times();
+
+		public static IEnumerable<T> Times<T>(this int count, Func<T> func) => count.Times().Select(x => func());
+		public static IEnumerable<T> Times<T>(this uint count, Func<T> func) => ((int) count).Times(func);
 	}
 }
