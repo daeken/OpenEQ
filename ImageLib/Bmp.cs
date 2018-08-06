68b7dcaa00b5666ca76be243f1cde379d45428a0
Cody Brocious <cody.brocious@gmail.com>
2018-08-05 21:50:34 -0400

    Added BMP support and made character textures come over.

---
+++ ImageLib/Bmp.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Bmp;
+using SixLabors.ImageSharp.PixelFormats;
+
+namespace ImageLib {
+	public static class Bmp {
+		public static Image Load(byte[] data) {
+			using(var ms = new MemoryStream(data))
+				using(var simage = new BmpDecoder().Decode<Rgb24>(Configuration.Default, ms))
+					return new Image(ColorMode.Rgb, (simage.Width, simage.Height), simage.Frames[0].SavePixelData());
+		}
+	}
+}
