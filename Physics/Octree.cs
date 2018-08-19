0d119d58c7fbdb13fc002c76da4260e3743933f3
Cody Brocious <cody.brocious@gmail.com>
2018-08-19 15:27:14 -0400

    Start of physics.

---
+++ Physics/Octree.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+
+namespace Physics {
+	public class Octree {
+		/*
+		 * A -- min, min, min
+		 * B -- max, min, min
+		 * C -- min, max, min
+		 * D -- max, max, min
+		 * E -- min, min, max
+		 * F -- max, min, max
+		 * G -- min, max, max
+		 * H -- max, max, max
+		 */
+		public readonly Octree A, B, C, D, E, F, G, H;
+
+		public Mesh Leaf;
+
+		public Octree(Mesh mesh, int maxTrisPerLeaf) {
+			if(mesh.Triangles.Count <= maxTrisPerLeaf) {
+				Leaf = mesh;
+				return;
+			}
+
+			Mesh BuildSub(AABB bb) =>
+				new Mesh(mesh.Triangles.Where(bb.Contains));
+
+			var c = mesh.BoundingBox.Min;
+			var hs = mesh.BoundingBox.Size / 2;
+			var nodes = new[] {
+				new AABB(c, hs), 
+				new AABB(new Vector3(c.X + hs.X, c.Y, c.Z), hs), 
+				new AABB(new Vector3(c.X, c.Y + hs.Y, c.Z), hs), 
+				new AABB(new Vector3(c.X + hs.X, c.Y + hs.Y, c.Z), hs),
+				
+				new AABB(new Vector3(c.X, c.Y, c.Z + hs.Z), hs), 
+				new AABB(new Vector3(c.X + hs.X, c.Y, c.Z + hs.Z), hs), 
+				new AABB(new Vector3(c.X, c.Y + hs.Y, c.Z + hs.Z), hs), 
+				new AABB(new Vector3(c.X + hs.X, c.Y + hs.Y, c.Z + hs.Z), hs)
+			}.AsParallel().AsOrdered().Select(BuildSub).AsSequential().Select(x => new Octree(x, maxTrisPerLeaf)).ToList();
+			
+			A = nodes[0];
+			B = nodes[1];
+			C = nodes[2];
+			D = nodes[3];
+			E = nodes[4];
+			F = nodes[5];
+			G = nodes[6];
+			H = nodes[7];
+		}
+	}
+}
---
+++ Physics/World.cs
@@ -0,0 +1,4 @@
+﻿namespace Physics {
+	public class World {
+	}
+}
