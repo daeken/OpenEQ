020d1c58d5f1c3530f6f98b1d774ae186e6b6aa8
Cody Brocious <cody.brocious@gmail.com>
2018-08-23 04:20:29 -0400

    Removed Jitter.  Implemented octree-ray and triangle-ray intersections.  Added bind-to-ground style movement.

---
+++ CollisionManager/AABB.cs
@@ -0,0 +1,49 @@
+﻿using System.Numerics;
+
+namespace CollisionManager {
+	public struct AABB {
+		public readonly Vector3 Min, Max;
+		public readonly Vector3 Size;
+		public readonly Vector3 Center;
+
+		public AABB(Vector3 min, Vector3 size) {
+			Min = min;
+			Max = min + size;
+			Size = size;
+			Center = min + size / 2;
+		}
+
+		// TODO: Make this handle cases where a portion of a triangle is within this AABB!
+		public bool Contains(Triangle tri) =>
+			Contains(tri.A) || Contains(tri.B) || Contains(tri.C);
+
+		public bool Contains(Vector3 point) =>
+			Min.X <= point.X && Min.Y <= point.Y && Min.Z <= point.Z && 
+			Max.X >= point.X && Max.Y >= point.Y && Max.Z >= point.Z;
+
+		public bool IntersectedBy(Vector3 origin, Vector3 direction) {
+			if(Contains(origin)) return true;
+			
+			var tmin = (Min.X - origin.X) / direction.X;
+			var tmax = (Max.X - origin.X) / direction.X;
+			if(tmin > tmax) { var temp = tmin; tmin = tmax; tmax = temp; }
+			
+			var tymin = (Min.Y - origin.Y) / direction.Y;
+			var tymax = (Max.Y - origin.Y) / direction.Y;
+			if(tymin > tymax) { var temp = tymin; tymin = tymax; tymax = temp; }
+
+			if(tmin > tymax || tymin > tmax) return false;
+
+			if(tymin > tmin) tmin = tymin;
+			if(tymax < tmax) tmax = tymax;
+			
+			var tzmin = (Min.Z - origin.Z) / direction.Z;
+			var tzmax = (Max.Z - origin.Z) / direction.Z;
+			if(tzmin > tzmax) { var temp = tzmin; tzmin = tzmax; tzmax = temp; }
+
+			return tmin <= tzmax && tzmin <= tmax;
+		}
+
+		public override string ToString() => $"AABB(Min={Min}, Max={Max})";
+	}
+}
---
+++ CollisionManager/Triangle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Numerics;
+
+namespace CollisionManager {
+	public struct Triangle {
+		const float Epsilon = 0.00001f;
+		
+		public Vector3 A, B, C;
+		public Vector3 Normal;
+
+		public Vector3[] AsArray => new[] { A, B, C };
+
+		public Triangle(Vector3 a, Vector3 b, Vector3 c) {
+			A = a;
+			B = b;
+			C = c;
+			Normal = Vector3.Cross(b - a, c - a);
+		}
+
+		// Moller-Trumbore
+		public (Vector3, float)? FindIntersection(Vector3 origin, Vector3 direction) {
+			var edge1 = B - A;
+			var edge2 = C - A;
+			var h = Vector3.Cross(direction, edge2);
+			var a = Vector3.Dot(edge1, h);
+			if(a > -Epsilon && a < Epsilon) return null; // If it's < Epsilon, it's facing away from us; we can cull later if needed
+			var f = 1 / a;
+			var s = origin - A;
+			var u = f * Vector3.Dot(s, h);
+			if(u < 0 || u > 1) return null;
+			var q = Vector3.Cross(s, edge1);
+			var v = f * Vector3.Dot(direction, q);
+			if(v < 0 || u + v > 1) return null;
+			var t = f * Vector3.Dot(edge2, q);
+			if(t <= Epsilon) return null;
+			return (origin + direction * t, t);
+		}
+	}
+}
