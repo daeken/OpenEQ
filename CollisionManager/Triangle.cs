b51b12d4c0f395e0b4ddc2fd8a9c3747fc5d3c56
Cody Brocious <cody.brocious@gmail.com>
2018-08-25 19:00:56 -0400

    Added temporary, broken octree-triangle splitting code.  Going to redo this.

--- CollisionManager/Triangle.cs
+++ CollisionManager/Triangle.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Numerics;
 using OpenEQ.Common;
 
@@ -10,6 +11,16 @@ namespace CollisionManager {
 		public Vector3 Normal;
 
 		public Vector3[] AsArray => new[] { A, B, C };
+		
+		public AABB BoundingBox {
+			get {
+				var min = Vector3.Min(A, Vector3.Min(B, C));
+				return new AABB(
+					min,
+					Vector3.Max(A, Vector3.Max(B, C)) - min
+				);
+			}
+		}
 
 		public Triangle(Vector3 a, Vector3 b, Vector3 c) {
 			A = a;
@@ -36,5 +47,47 @@ namespace CollisionManager {
 			if(t <= Epsilon) return null;
 			return (origin + direction * t, t);
 		}
+
+		public IEnumerable<Triangle> Split(Plane plane) {
+			var d1 = Vector3.Dot(A, plane.Normal) - plane.Distance;
+			var d2 = Vector3.Dot(B, plane.Normal) - plane.Distance;
+			var d3 = Vector3.Dot(C, plane.Normal) - plane.Distance;
+
+			if(d1 * d2 < 0) return Slice(A, B, C, d1, d2, d3);
+			if(d1 * d3 < 0) return Slice(C, A, B, d3, d1, d2);
+			if(d2 * d3 < 0) return Slice(B, C, A, d2, d3, d1);
+			return new[] { this };
+		}
+
+		static IEnumerable<Triangle> Slice(Vector3 a, Vector3 b, Vector3 c, float d1, float d2, float d3) {
+			var ab = a + d1 / (d1 - d2) * (b - a);
+			if(d1 < 0) {
+				if(d3 < 0) {
+					var bc = b + d2 / (d2 - d3) * (c - b);
+					yield return new Triangle(b, bc, ab);
+					yield return new Triangle(bc, c, a);
+					yield return new Triangle(ab, bc, a);
+				} else {
+					var ac = a + d1 / (d1 - d3) * (c - a);
+					yield return new Triangle(a, ab, ac);
+					yield return new Triangle(ab, b, c);
+					yield return new Triangle(ac, ab, c);
+				}
+			} else {
+				if(d3 < 0) {
+					var ac = a + d1 / (d1 - d3) * (c - a);
+					yield return new Triangle(a, ab, ac);
+					yield return new Triangle(ac, ab, b);
+					yield return new Triangle(b, c, ac);
+				} else {
+					var bc = b + d2 / (d2 - d3) * (c - b);
+					yield return new Triangle(b, bc, ab);
+					yield return new Triangle(a, ab, bc);
+					yield return new Triangle(c, a, bc);
+				}
+			}
+		}
+
+		public override string ToString() => $"Triangle({A}, {B}, {C}, ({Normal}))";
 	}
 }
