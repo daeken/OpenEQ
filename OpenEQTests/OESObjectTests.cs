edb55275835125e07208e4aec15f5618b6ce37f3
John H. Freeman <johnherschelfreeman2013@gmail.com>
2023-08-20 00:58:42 -0400

    Added another test

--- OpenEQTests/OESObjectTests.cs
+++ OpenEQTests/OESObjectTests.cs
@@ -1,3 +1,6 @@
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+
 namespace OpenEQTests
 {
     public class OESObjectTests
@@ -21,6 +24,14 @@ namespace OpenEQTests
         {
             OpenEQ.Common.OESObject oESOBJ = new OpenEQ.Common.OESObject();
             Assert.That(oESOBJ.Name, Is.Not.EqualTo(oESOBJ.ToString()));
+        }
+
+        [Test]
+        public void Test3()
+        {
+            Regex regex = new Regex(@"OESObject\(Name=(?<name>\S+)\)");
+            Match m = regex.Match(oESObject.ToString());
+            Assert.That(oESObject.Name, Is.EqualTo(m.Groups["name"].Value));
         }
     }
 }
